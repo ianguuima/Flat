32dc1ad488367d9c409b82c4bc50b771701c8422
SasukeMCHC <africancontato@gmail.com>
2019-07-11 10:13:08 -0300

    Added static methods to Flats class

--- src/main/java/br/com/infernalia/flat/Flats.java
+++ src/main/java/br/com/infernalia/flat/Flats.java
@@ -2,20 +2,26 @@ package br.com.infernalia.flat;
 
 import br.com.infernalia.flat.impl.FlatArrayList;
 import br.com.infernalia.flat.impl.FlatHashSet;
-
-import java.util.Arrays;
-
 import static java.util.Arrays.*;
 
 public class Flats {
 
+   public static <T> Flat<T> emptyList(){
+      return new FlatArrayList<>();
+   }
+
+   public static <T> Flat<T> emptySet(){
+      return new FlatHashSet<>();
+   }
+
+   @SafeVarargs
    public static <T> Flat<T> mutableListOf(T... array){
       return new FlatArrayList<>(asList(array));
    }
 
+   @SafeVarargs
    public static <T> Flat<T> mutableSetOf(T... array){
       return new FlatHashSet<>(asList(array));
    }
-
 
 }
